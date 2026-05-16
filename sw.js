7e427435c5d3cf270dd2f23910dea3ee66175fe1
ncomas <nicocomas15@gmail.com>
2026-05-16 20:39:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,37 @@
-const CACHE_NAME = "mundial-cache-v2";
+const CACHE_NAME = "mundial-cache-v3";
 
 const urlsToCache = [
     "./",
-    "./index.html"
+    "./index.html",
+    "./manifest.json"
 ];
 
 self.addEventListener("install", event=>{
-
     event.waitUntil(
-
         caches.open(CACHE_NAME)
             .then(cache=>cache.addAll(urlsToCache))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event=>{
+    event.waitUntil(
+        caches.keys().then(keys=>{
+            return Promise.all(
+                keys
+                    .filter(key=>key !== CACHE_NAME)
+                    .map(key=>caches.delete(key))
+            );
+        })
+    );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event=>{
-
     event.respondWith(
-
         caches.match(event.request)
             .then(response=>{
-
                 return response || fetch(event.request);
             })
     );
